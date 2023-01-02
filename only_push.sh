3ef3917d718bac2bb9a484bc43844e4ecea36bd7
kirusnovar <kirus@kirus.icu>
2023-01-02 01:19:23 +0000

    message from docker

--- only_push.sh
+++ only_push.sh
@@ -4,4 +4,4 @@ git config --global user.email "kirus@kirus.icu" && git config --global user.nam
 echo commiting
 git add . && git commit -m "message from docker" 
 echo pushing
-git push https://github.com/kirusnovar/mine2.git master
+git push https://kirusnovar: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@github.com/kirusnovar/mine2.git master
