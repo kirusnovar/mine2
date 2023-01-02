7c21d2e32fc04687b68c64d537c820d00ea6c4f6
kirusnovar <kirus@kirus.icu>
2023-01-02 01:28:28 +0000

    message from docker

--- only_push.sh
+++ only_push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 git config --global user.email "kirus@kirus.icu" && git config --global user.name "kirusnovar" 
 
+
 echo commiting
 git add . && git commit -m "message from docker" 
 echo pushing
-git push https://kirusnovar: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@github.com/kirusnovar/mine2.git master
+git push https://@github.com/kirusnovar/mine2.git master
+
--- push_service.sh
+++ push_service.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo push_service_started
+git push https://kirusnovar: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@github.com/kirusnovar/mine2.git master
+git config credential.helper store
 
 
 while true
