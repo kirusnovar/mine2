5464d999f006bf7c9a7e17e7cdf34498eabbd756
kirusnovar <121721282+kirusnovar@users.noreply.github.com>
2023-01-03 03:54:52 +0530

    Update only_push.sh

--- only_push.sh
+++ only_push.sh
@@ -4,7 +4,7 @@
 git config --global user.email "kirus@kirus.icu" && git config --global user.name "kirusnovar"
 echo commiting
 git add . && git commit -m "message from docker" 
-echo "pushing"
-git push git@github.com:kirusnovar/mine2.git master -yes
+echo "_____________pushing"
+git push git@github.com:kirusnovar/mine2.git master
 
 
