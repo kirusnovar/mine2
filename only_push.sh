138d1f61757921efa00d308a6920b5c6a8757691
kirusnovar <kirus@kirus.icu>
2023-01-02 01:31:50 +0000

    message from docker

--- only_push.sh
+++ only_push.sh
@@ -5,5 +5,5 @@
 echo commiting
 git add . && git commit -m "message from docker" 
 echo pushing
-git push https://@github.com/kirusnovar/mine2.git master
+git push https://github.com/kirusnovar/mine2.git master
 
