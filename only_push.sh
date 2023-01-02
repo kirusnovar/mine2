1c9845cd50c4ccaea14536ccd41f8500049fe323
kirusnovar <kirus@kirus.icu>
2023-01-02 01:42:57 +0000

    message from docker

--- only_push.sh
+++ only_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 
-
+git config --global user.email "kirus@kirus.icu" && git config --global user.name "kirusnovar"
 echo commiting
 git add . && git commit -m "message from docker" 
 echo "pushing"
